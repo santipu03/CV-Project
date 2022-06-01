4a63b76d2bba358ac9e9d3a96dab23e00fbc25bb
santipu_ <santipujolsau@gmail.com>
2022-06-01 17:52:00 +0200

    Converted ExperienceItems class component into functional

--- src/components/ExperienceItems.js
+++ src/components/ExperienceItems.js
@@ -5,100 +5,98 @@ import { Input } from './Styles/Input.style'
 import { Form } from './Styles/Form.style'
 import styled from 'styled-components'
 
-export default class ExperienceItems extends React.Component {
-  render () {
-    if (!this.props.isSubmitted) {
-      return (
-        <>
-          <Form>
-            <div>
-              <label htmlFor="companyName">Company Name</label>
-              <Input
-                type={'text'}
-                placeholder="Microsoft"
-                id="companyName"
-                value={this.props.companyName}
-                onChange={(e) => this.props.onInputChange(e, this.props.id)}
-              ></Input>
-            </div>
-            <div>
-              <label htmlFor="companyPosition">Position Title</label>
-              <Input
-                type={'text'}
-                placeholder="Senior Developer"
-                id="companyPosition"
-                value={this.props.companyPosition}
-                onChange={(e) => this.props.onInputChange(e, this.props.id)}
-              ></Input>
-            </div>
-            <div>
-              <label htmlFor="firstDayJob">From:</label>
-              <Input
-                type={'month'}
-                id="firstDayJob"
-                value={this.props.firstDayJob}
-                onChange={(e) => this.props.onInputChange(e, this.props.id)}
-              ></Input>
-            </div>
-            <div>
-              <label htmlFor="lastDayJob">To:</label>
-              <Input
-                type={'month'}
-                id="lastDayJob"
-                value={this.props.lastDayJob}
-                onChange={(e) => this.props.onInputChange(e, this.props.id)}
-              ></Input>
-            </div>
-          </Form>
-          <ButtonWrapper>
-            <Button
-              type="button"
-              onClick={() => this.props.onDeleteItem(this.props.id)}
-              bgColor="delete"
-            >
-              Delete
-            </Button>
-          </ButtonWrapper>
-        </>
-      )
-    } else if (
-      this.props.isSubmitted &&
-      this.props.firstDayJob &&
-      this.props.lastDayJob
-    ) {
-      // The date prop has to have some value so date-fns library don't throw errors
-      return (
-        <SubmittedItemWrapper>
+export default function ExperienceItems (props) {
+  if (!props.isSubmitted) {
+    return (
+      <>
+        <Form>
           <div>
-            <strong>
-              {format(new Date(this.props.firstDayJob), 'MMM yyyy')} -&nbsp;
-              {format(new Date(this.props.lastDayJob), 'MMM yyyy')}
-            </strong>
+            <label htmlFor="companyName">Company Name</label>
+            <Input
+              type={'text'}
+              placeholder="Microsoft"
+              id="companyName"
+              value={props.companyName}
+              onChange={(e) => props.onInputChange(e, props.id)}
+            ></Input>
           </div>
-          <AlignedRightWrapper>
-            <div>
-              <strong>{this.props.companyPosition}</strong>
-            </div>
-            <div>{this.props.companyName}</div>
-          </AlignedRightWrapper>
-        </SubmittedItemWrapper>
-      )
-    } else {
-      // If the date prop has no value, return 'Date'
-      return (
-        <SubmittedItemWrapper>
           <div>
-            <strong>Date -&nbsp; Date</strong>
+            <label htmlFor="companyPosition">Position Title</label>
+            <Input
+              type={'text'}
+              placeholder="Senior Developer"
+              id="companyPosition"
+              value={props.companyPosition}
+              onChange={(e) => props.onInputChange(e, props.id)}
+            ></Input>
           </div>
-          <AlignedRightWrapper>
-            <div>
-              <strong>{this.props.companyPosition}</strong>
-            </div>
-            <div>{this.props.companyName}</div>
-          </AlignedRightWrapper>
-        </SubmittedItemWrapper>
-      )
-    }
+          <div>
+            <label htmlFor="firstDayJob">From:</label>
+            <Input
+              type={'month'}
+              id="firstDayJob"
+              value={props.firstDayJob}
+              onChange={(e) => props.onInputChange(e, props.id)}
+            ></Input>
+          </div>
+          <div>
+            <label htmlFor="lastDayJob">To:</label>
+            <Input
+              type={'month'}
+              id="lastDayJob"
+              value={props.lastDayJob}
+              onChange={(e) => props.onInputChange(e, props.id)}
+            ></Input>
+          </div>
+        </Form>
+        <ButtonWrapper>
+          <Button
+            type="button"
+            onClick={() => props.onDeleteItem(props.id)}
+            bgColor="delete"
+          >
+            Delete
+          </Button>
+        </ButtonWrapper>
+      </>
+    )
+  } else if (
+    props.isSubmitted &&
+    props.firstDayJob &&
+    props.lastDayJob
+  ) {
+    // The date prop has to have some value so date-fns library don't throw errors
+    return (
+      <SubmittedItemWrapper>
+        <div>
+          <strong>
+            {format(new Date(props.firstDayJob), 'MMM yyyy')} -&nbsp;
+            {format(new Date(props.lastDayJob), 'MMM yyyy')}
+          </strong>
+        </div>
+        <AlignedRightWrapper>
+          <div>
+            <strong>{props.companyPosition}</strong>
+          </div>
+          <div>{props.companyName}</div>
+        </AlignedRightWrapper>
+      </SubmittedItemWrapper>
+    )
+  } else {
+    // If the date prop has no value, return 'Date'
+    return (
+      <SubmittedItemWrapper>
+        <div>
+          <strong>Date -&nbsp; Date</strong>
+        </div>
+        <AlignedRightWrapper>
+          <div>
+            <strong>{props.companyPosition}</strong>
+          </div>
+          <div>{props.companyName}</div>
+        </AlignedRightWrapper>
+      </SubmittedItemWrapper>
+    )
   }
 }
 
