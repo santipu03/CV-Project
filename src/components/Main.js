d0aefa4e3a25935b934dd64f1da26e0be2af9883
santipu_ <santipujolsau@gmail.com>
2022-06-01 18:09:38 +0200

    Converted Main.js class component into functional

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import GeneralSection from './GeneralSection'
 import ExperienceSection from './ExperienceSection'
 import EducationSection from './EducationSection'
@@ -6,33 +6,24 @@ import CardFooter from './CardFooter'
 import { Card } from './Styles/Card.style'
 import { MainContainer } from './Styles/MainContainer.style'
 
-export default class Main extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      isSubmitted: false
-    }
+export default function Main (props) {
+  const [isSubmitted, setIsSubmitted] = useState(false)
+
+  const handleSubmit = () => {
+    setIsSubmitted(!isSubmitted)
   }
 
-  handleSubmit () {
-    this.setState((previousState) => ({
-      isSubmitted: !previousState.isSubmitted
-    }))
-  }
-
-  render () {
-    return (
-      <MainContainer>
-        <Card>
-          <GeneralSection isSubmitted={this.state.isSubmitted} />
-          <ExperienceSection isSubmitted={this.state.isSubmitted} />
-          <EducationSection isSubmitted={this.state.isSubmitted} />
-          <CardFooter
-            onSubmitBtn={this.handleSubmit.bind(this)}
-            isSubmitted={this.state.isSubmitted}
-          />
-        </Card>
-      </MainContainer>
-    )
-  }
+  return (
+    <MainContainer>
+      <Card>
+        <GeneralSection isSubmitted={isSubmitted} />
+        <ExperienceSection isSubmitted={isSubmitted} />
+        <EducationSection isSubmitted={isSubmitted} />
+        <CardFooter
+          onSubmitBtn={handleSubmit.bind(this)}
+          isSubmitted={isSubmitted}
+        />
+      </Card>
+    </MainContainer>
+  )
 }
